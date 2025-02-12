b30d12ed4ff2d161762fd9fe238ac4d6495fe59d
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2025-02-12 17:02:34 +0100

    feat: adopt docker for deployment

--- mqttMessageForwarder/main.py
+++ mqttMessageForwarder/main.py
@@ -1,19 +1,24 @@
 import asyncio
 import json
 import websockets
-from mqtt_wrapper import MQTTWrapper
-import sys
+import os
+import paho.mqtt.client as mqtt
 
-baseHeartRate = int(sys.argv[1])
-maxArousalDeviation = 10
+WEBSOCKET_PORT = 7890
+
+MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
+MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
+MQTT_TOPIC = os.getenv("MQTT_TOPIC", "NP_UNIPR_AROUSAL")
+
+MAX_AROUSAL_DEVIATION = int(os.getenv("MAX_AROUSAL_DEVIATION", 10))
+BASE_HEART_RATE = int(os.getenv("BASE_HEART_RATE", 60))
+
 def heartRateToArousal(heartRate):
-    arousal = (1/maxArousalDeviation**2) * (heartRate - baseHeartRate)**2
+    arousal = (1/MAX_AROUSAL_DEVIATION**2) * (heartRate - BASE_HEART_RATE)**2
     return arousal if arousal <= 1 else 1
 
-client = MQTTWrapper.fromJsonConfig(sys.path[0] + "/mqttConfig.json")
-
 async def main():
-    async with websockets.serve(handleMessage, '0.0.0.0', 7890):
+    async with websockets.serve(handleMessage, '0.0.0.0', WEBSOCKET_PORT):
         await asyncio.Future()
 
 async def handleMessage(websocket):
@@ -21,9 +26,16 @@ async def handleMessage(websocket):
         message = json.loads(message)
         arousal = heartRateToArousal(int(message["heartRate"]))
         print(f"HR {message['heartRate']} -> arousal {arousal}")
-        client.publish(json.dumps({ "arousal": arousal }))
-        
+        client.publish(
+            topic=MQTT_TOPIC,
+            payload=json.dumps({ "arousal": arousal })
+        )
 
-asyncio.run(main())
+if __name__ == "__main__":
+    print(f"Connecting to MQTT broker {MQTT_BROKER} on port {MQTT_PORT} and topic {MQTT_TOPIC}")
+    print(f"Max arousal deviation: {MAX_AROUSAL_DEVIATION}, base heart rate: {BASE_HEART_RATE}")
 
+    client = mqtt.Client()
+    client.connect(host=MQTT_BROKER, port=MQTT_PORT)
 
+    asyncio.run(main())
